175262463abf2e5ce666aa4413821662bb974c6f
Jay-Zzy <zhengzyofficial@gmail.com>
2021-07-01 13:14:02 +0800

    update

--- Random Forest/eg.FillNaN.py
+++ Random Forest/eg.FillNaN.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
 from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
 
 dataset = load_boston()
-# print(dataset.data.shape)
 x_full, y_full = dataset.data, dataset.target
 n_samples = x_full.shape[0]
 n_features = x_full.shape[1]
@@ -20,17 +20,45 @@ n_missing_samples = int(np.floor(n_samples * n_features * missing_rate))  # np.f
 missing_features = rng.randint(0, n_features, n_missing_samples)
 missing_samples = rng.randint(0, n_samples, n_missing_samples)
 x_missing = x_full.copy()
-y_missing = y_full.copy()
 x_missing[missing_samples, missing_features] = np.nan
 x_missing = pd.DataFrame(x_missing)
-print(x_missing, '\n', x_missing.isnull().sum().sum())
+print(x_missing, '\n缺失值共有：', x_missing.isnull().sum().sum())
 
 # 均值填补
 imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
 x_missing_mean = imp_mean.fit_transform(x_missing)  # fit_transform()训练+导出
-print(pd.DataFrame(x_missing_mean))
+# print(pd.DataFrame(x_missing_mean))
 
 # 0值填补
 imp_0 = SimpleImputer(missing_values=np.nan, strategy='constant', fill_value=0)
 x_missing_0 = imp_0.fit_transform(x_missing)
-print(pd.DataFrame(x_missing_0))
+# print(pd.DataFrame(x_missing_0))
+
+# 随机森林填值
+sortindex = np.argsort(x_missing.isnull().sum(axis=0)).values  # 保留索引排序，返回索引
+# print(sortindex)
+for i in sortindex:
+    df = x_missing
+    fillc = df.iloc[:, i]  # 从缺失值最少的一列开始
+    df = pd.concat([df.iloc[:, df.columns != i], pd.DataFrame(y_full)], axis=1)  # 构建新特征矩阵
+    df_0 = SimpleImputer(missing_values=np.nan, strategy='constant', fill_value=0).fit_transform(df)
+    Ytrain = fillc[fillc.notnull()]
+    Xtrain = df_0[Ytrain.index, :]
+    Ytest = fillc[fillc.isnull()]  # 这里只是为了知道要预测的值的位置(索引)
+    Xtest = df_0[Ytest.index, :]
+
+    rfc = RandomForestRegressor(n_estimators=100).fit(Xtrain, Ytrain)
+    Ypredict = rfc.predict(Xtest)
+    # 对列填值(loc是根据index来索引，而iloc是根据行号来索引)
+    x_missing.loc[x_missing.iloc[:, i].isnull(), i] = Ypredict
+# print(x_missing, '\n缺失值共有：', x_missing.isnull().sum().sum())
+
+# 多方法对比
+X = [x_full, x_missing_mean, x_missing_0, x_missing]
+mse = []
+for x in X:
+    estimator = RandomForestRegressor(random_state=0, n_estimators=100)
+    # 5折验证并求平均
+    scores = cross_val_score(estimator, x, y_full, scoring='neg_mean_squared_error', cv=5).mean()
+    mse.append(scores * -1)
+print('\n', *zip(['x_full', 'x_missing_mean', 'x_missing_0', 'x_missing'], mse))
